03b28be62ef9cf629240081110af438a8df7e403
EDK <50658728+EEDK@users.noreply.github.com>
2023-04-25 15:17:18 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 620 ms, Memory: 238880 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,34 @@
+import sys
+
+
+def solution(sangGen, numCard):
+    answer = []
+
+    dict = {}
+    for card in sangGen:
+        try:
+            dict[card] += 1
+        except:
+            dict[card] = 1
+
+    for card in numCard:
+        try:
+            answer.append(dict[card])
+        except:
+            answer.append(0)
+
+    return answer
+
+
+def printArr(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+
+
+n = int(sys.stdin.readline())
+arr1 = list(map(int, sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+arr2 = list(map(int, sys.stdin.readline().split()))
+
+answer = solution(arr1, arr2)
+printArr(answer)
