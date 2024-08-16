4084876ce19aad2014f6ab37ec33759a3d699406
EDK <50658728+EEDK@users.noreply.github.com>
2024-08-17 00:38:56 +0900

    [Silver III] Title: 소수 구하기, Time: 1840 ms, Memory: 5872 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int M, N;
+
+	cin >> M >> N;
+
+	vector<int> A(N + 1, 0);
+
+	for (int i = 2; i <= N; i++)
+	{
+		A[i] = i;
+	}
+
+	for (int i = 2; i <= sqrt(N); i++)
+	{
+		if (A[i] == 0)
+		{
+			continue;
+		}
+
+		for (int j = i * i; j <= N; j += i) 
+		{
+			A[j] = 0;
+		}
+	}
+
+	for (int i = M; i <= N; i++)
+	{
+		if (A[i] != 0)
+		{
+			cout << A[i] << endl;
+		}
+	}
+
+	return 0;
+}
