ff2b54ef65e981aae4f68ef343c0617d0d51588b
EDK <50658728+EEDK@users.noreply.github.com>
2024-08-30 23:54:46 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.40 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,41 +1,49 @@
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-const vector<int> dx{-1, 1, 0, 0};
-const vector<int> dy{0, 0, -1, 1};
+const vector<int> dx{-1, 0, 1, 0};
+const vector<int> dy{0, -1, 0, 1};
 
-int solution(vector<vector<int>> maps) {
-  int n = maps.size();
-  int m = maps[0].size();
-  vector<vector<int>> dist(n, vector<int>(m, -1));
+static int n;
+static int m;
 
-  queue<pair<int, int>> q;
-  q.push({0, 0});
-  dist[0][0] = 1;
-
-  while (!q.empty()) {
-    int x = q.front().first;
-    int y = q.front().second;
-    q.pop();
-
-    for (int i = 0; i < 4; i++) {
-      int nx = x + dx[i];
-      int ny = y + dy[i];
-
-      if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
-        continue;
-      }
-      if (maps[nx][ny] == 0) {
-        continue;
-      }
-      if (dist[nx][ny] == -1) {
-        dist[nx][ny] = dist[x][y] + 1;
-        q.push({nx, ny});
-      }
+int bfs(vector<vector<int> > maps, vector<vector<int>> &Visited){
+    
+    queue<pair<int, int>> Queue;
+    
+    Queue.push({0, 0});
+    Visited[0][0] = 1;
+    
+    while(!Queue.empty()){
+        int x = Queue.front().first;
+        int y = Queue.front().second;
+        Queue.pop();
+        
+        for(int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+            
+            else if(maps[nx][ny] == 0 || Visited[nx][ny] > 0) continue;
+            
+            Visited[nx][ny] = Visited[x][y] + 1;
+            Queue.push({nx, ny});
+        }
+        
     }
-  }
+        
+    return Visited[n-1][m-1];
+}
 
-  int answer = dist[n - 1][m - 1];
-  return answer == -1 ? -1 : answer;
+int solution(vector<vector<int> > maps)
+{
+    int answer = 0;
+    n = maps.size();
+    m = maps[0].size();
+    
+    vector<vector<int>> Visited(n, vector<int>(m, 0));
+    
+    return bfs(maps, Visited) == 0 ? -1 : bfs(maps, Visited);
 }
