d763a83a12713f3a814253ce7b76bd10f71702f3
EDK <50658728+EEDK@users.noreply.github.com>
2024-11-14 01:49:32 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define FASTIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FOR(i, n) for(int i = 0; i < (n); ++i)
+#define INF (~0U >> 2)
+#define PB push_back
+#define MOD 1000000007
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+int dy[4] = {1, 0, -1, 0};
+int dx[4] = {0, 1, 0, -1};
+
+int main()
+{
+    FASTIO
+
+    int N;
+    cin >> N;
+
+    cout << pow(2, N) << endl;
+
+    return 0;
+}
