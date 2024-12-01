352256aa45dd4e139cbe362be7e08aca958a2938
EDK <50658728+EEDK@users.noreply.github.com>
2024-12-01 21:31:45 +0900

    [level 2] Title: 큰 수 만들기, Time: 4.58 ms, Memory: 5.63 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = ""; 
+    int size = number.size() - k;
+    
+    for (char digit : number) {
+        while (!answer.empty() && k > 0 && answer.back() < digit) {
+            answer.pop_back();
+            k--;
+        }
+        answer.push_back(digit);
+    }
+    
+    answer = answer.substr(0, size);
+    return answer;
+}
